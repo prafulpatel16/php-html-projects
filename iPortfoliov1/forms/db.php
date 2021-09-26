c0b0d37976ef024212f4a69ac3e13e792fc5ac01
Praful Patel <praful1611@outlook.com>
2021-09-25 23:03:29 -0600

    final commit with updated database

--- iPortfoliov1/forms/db.php
+++ iPortfoliov1/forms/db.php
@@ -1,29 +1,12 @@
 <?php
-    $servername='database-1.ctyvngdrcnhr.us-east-1.rds.amazonaws.com';
+    $servername='mysql2021.cntikk0jg8xf.ca-central-1.rds.amazonaws.com';
     $username='admin';
-    $password='mypassword';
-    $dbname = "test";
+    $password='admin123456';
+    $dbname = "contacts";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
         }
 ?>
 
-<?php
-include_once 'db.php';
- if(isset($_POST['submit']))
- {    
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $subject = $_POST['subject'];
-            $sql = "INSERT INTO test1(name,email,subject)
-      VALUES ('$name','$email','$subject')";
-      if (mysqli_query($conn, $sql)) {
-         echo "New record has been added successfully !";
-      } else {
-         echo "Error: " . $sql . ":-" . mysqli_error($conn);
-      }
-      mysqli_close($conn);
- }
- 
-?>
+
